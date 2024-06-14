15b40ef430c3f50a38dcc5d5ca32eaf06416fd84
SharnuSharath123 <sharnuburli143@gmail.com>
2024-06-14 13:48:17 +0530

    practice programs

---
+++ dempo.js
@@ -0,0 +1,31 @@
+// let data = 7;
+// while(data>=0){
+//     if(data<=5){
+//         data--;
+//         continue;
+//     }
+//     console.log(data)
+//     data--;
+// }
+
+
+// for(let i=1; ; i++){
+//     let data = i*5
+//     console.log(data)
+//     if(data>=30){
+//         break;
+//     }
+// }
+
+// const str = "GeeksForGeeks"
+// for(let i=0; i<str.length;i++){
+//     console.log(i)
+// }
+// console.log(str.length)
+
+let sum = 0
+const n = 100
+for(let i=1;i<=n;i++){
+    sum+=i;
+}
+console.log('sum:', sum)
